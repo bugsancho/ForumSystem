0ee0e35c02902bc61877f79fdc9322a94712b8b7
Alexander Todorov <bugsancho@gmail.com>
2018-05-11 20:19:22 +0300

    Removing unnecessary account actions/models

--- ForumSystem.Web/Models/AccountViewModels.cs
+++ ForumSystem.Web/Models/AccountViewModels.cs
@@ -1,48 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ForumSystem.Web.Models
 {
     // Models returned by AccountController actions.
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-
-        [Display(Name = "Hometown")]
-        public string Hometown { get; set; }
-    }
-
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
-
-    public class SendCodeViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
-        public bool RememberMe { get; set; }
-    }
-
-    public class VerifyCodeViewModel
-    {
-        [Required]
-        public string Provider { get; set; }
-
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-        public string ReturnUrl { get; set; }
-
-        [Display(Name = "Remember this browser?")]
-        public bool RememberBrowser { get; set; }
-
-        public bool RememberMe { get; set; }
-    }
 
     public class ForgotViewModel
     {
