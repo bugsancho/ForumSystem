42aad5ce0bf50138a8b4bcece910efbe94e171be
Alexander Todorov <bugsancho@gmail.com>
2018-05-12 19:27:03 +0300

    Adding ForumThreadsService

---
+++ src/ForumSystem.Core/Services/ForumThreadsService.cs
@@ -0,0 +1,42 @@
+﻿namespace ForumSystem.Core.Services
+{
+    using System.Collections.Generic;
+
+    using ForumSystem.Core.Data;
+    using ForumSystem.Core.Entities;
+
+    public class ForumThreadsService : IForumThreadsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ForumThreadsService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IReadOnlyCollection<ForumThread> GetAll()
+        {
+            return _unitOfWork.ForumThreads.All();
+        }
+
+        public ForumThread GetById(int id)
+        {
+            return _unitOfWork.ForumThreads.GetById(id);
+        }
+
+        public void Create(string title, ForumPost initialPost, User user = null)
+        {
+            ForumThread thread = new ForumThread
+            {
+                Title = title,
+                User = user,
+                UserId = user?.Id
+            };
+
+            thread.ForumPosts.Add(initialPost);
+
+            _unitOfWork.ForumThreads.Add(thread);
+            _unitOfWork.SaveChanges();
+        }
+    }
+}
---
+++ src/ForumSystem.Core/Services/IForumThreadsService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ForumSystem.Core.Entities;
+
+namespace ForumSystem.Core.Services
+{
+    public interface IForumThreadsService
+    {
+        IReadOnlyCollection<ForumThread> GetAll();
+
+        ForumThread GetById(int id);
+
+        void Create(string title, ForumPost initialPost, User user = null);
+    }
+}
