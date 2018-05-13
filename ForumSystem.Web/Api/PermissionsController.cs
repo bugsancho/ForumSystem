5339d2ead9e59a75f7d017d1a5acf4632283f546
Alexander Todorov <bugsancho@gmail.com>
2018-05-13 17:41:03 +0300

    Adding controller to enable listing of permissions to the client

---
+++ ForumSystem.Web/Api/PermissionsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ForumSystem.Web.Api
+{
+    using System.Threading.Tasks;
+
+    using ForumSystem.Core.Users;
+    using ForumSystem.Web.Models;
+
+    [Authorize]
+    public class PermissionsController : ApiController
+    {
+        private readonly IPermissionsService _permissionsService;
+
+        public PermissionsController(IPermissionsService permissionsService)
+        {
+            _permissionsService = permissionsService;
+        }
+
+        // GET api/<controller>
+        public async Task<PermissionsModel> Get()
+        {
+            string username = User.Identity.Name;
+            bool canEditThreads = await _permissionsService.CanEditThreads(username);
+            PermissionsModel result = new PermissionsModel()
+            {
+                Username = username,
+                CanEditThreads = canEditThreads
+            };
+
+            return result;
+        }
+
+
+    }
+}
---
+++ ForumSystem.Web/Models/PermissionsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ForumSystem.Web.Models
+{
+    public class PermissionsModel
+    {
+        public string Username { get; set; }
+
+        public bool CanEditThreads { get; set; }
+    }
+}
