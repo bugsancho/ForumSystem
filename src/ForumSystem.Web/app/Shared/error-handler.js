ca8fa47cdccf96d21191c50650cfd78b516ab337
Alexander Todorov <bugsancho@gmail.com>
2018-05-16 02:25:56 +0300

    Adding generic error handler for the angularjs app

---
+++ src/ForumSystem.Web/app/Shared/error-handler.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('forumSystem.shared')
+        .config(exceptionConfig);
+
+
+    function exceptionConfig($provide) {
+        $provide.decorator('$exceptionHandler', extendExceptionHandler);
+
+
+        function extendExceptionHandler($delegate, $injector) {
+            return function(exception, cause) {
+                $delegate(exception, cause);
+                const messageService = $injector.get('messageService');
+
+                // For the time being, just open a generic error dialog
+                messageService.showError();
+            };
+        }
+    }
+
+
+})();
---
+++ src/ForumSystem.Web/app/Shared/message-service.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('forumSystem.shared')
+        .factory('messageService', messageService);
+
+
+    function messageService($uibModal, $state) {
+        const service = {
+            showError: showError
+        };
+
+        return service;
+
+        function showError(error) {
+            // Currently we don't have a mechanism to extract a user-friendly message from exceptions.
+            // For the time being we show just a generic error message with the option to reload the page
+            var modalInstance = $uibModal.open({
+                templateUrl: "/error-message.html",
+                size: "sm",
+                controllerAs: '$ctrl',
+                controller: function () {
+                    var ctrl = this;
+                    ctrl.reloadPage = $state.reload;
+                }
+
+            });
+
+            modalInstance.result.then(function () {
+            },
+                function () {
+                    // When closing, the modal always throws an error.
+                    // If you don't provide an error handler function, closing or dismissing the modal, results in an unhandled exception
+                    console.log('Closing modal');
+                });
+        }
+    }
+})();
