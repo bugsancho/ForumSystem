359ad8cbcd2fc7fdbb0274b1f240c596461e96db
Alexander Todorov <bugsancho@gmail.com>
2018-05-16 10:31:16 +0300

    Adding validation to input parameters of thread statistics controller

--- src/ForumSystem.Web/Api/ThreadStatisticsController.cs
+++ src/ForumSystem.Web/Api/ThreadStatisticsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace ForumSystem.Web.Api
@@ -23,32 +22,12 @@ namespace ForumSystem.Web.Api
         // GET api/<controller>
         public async Task<IReadOnlyCollection<ThreadStatisticsResult>> Get([FromUri]ThreadStatisticsRequest statisticsRequest)
         {
-            if (statisticsRequest == null)
+            if (statisticsRequest == null || statisticsRequest.ThreadId <= 0 || statisticsRequest.AggregationInterval == StatisticsAggregationInterval.None)
             {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
+
             return await _statisticsService.Get(statisticsRequest);
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
         }
     }
 }
